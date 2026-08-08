e930835e0af7d0cf4f819f9fbdb48eb3bf5c4851
Zubayer <zubayer766@gmail.com>
2026-08-08 14:43:31 +0600

    added an user,tweet,like classes

---
+++ TwitterClone/TwitterClone.Domain/Entities/Tweet.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace TwitterClone.Domain.Entities
+{
+    public class Tweet
+    {
+        private Guid _id;
+        private Guid _authorId;
+        private string _content;
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+
+        public Guid AuthorId
+        {
+            get { return _authorId; }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
+    }
+}
---
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿
+namespace TwitterClone.Domain.Entities
+{
+    internal class User
+    {
+//this is fields:
+        private Guid _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+
+//this is constructor for making unique id (it will run one time,
+//so this unique id will generate only one time)
+//constructor name(User), and class name(User) will always same.
+        public User()
+        {
+            _id = Guid.NewGuid();
+        }
+
+//now we return this "_id",so that user get this id, when inspect.
+//must mention the type of "_id"-->Guid, here Id--> is the variable.
+//these three are-->"Property"
+        public Guid Id { get { return _id; } }
+        public string FirstName {
+            get { return _firstName; }
+            set { _firstName = value; }
+
+        }
+
+        public string LastName { 
+            get { return _lastName; }
+            set{ _lastName = value; }
+        }
+    }
+}
